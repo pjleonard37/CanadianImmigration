91f9da11e61355198ae1267ea021cae837079f57
pjleonard37 <pjleonard37@gmail.com>
2018-04-18 17:51:21 -0500

    Pulling

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ var svg = d3.select("svg"),
     width = +svg.attr("width"),
     height = +svg.attr("height");
 
-var stack = d3.stack().keys(d3.range(10)).offset(d3.stackOffsetWiggle), layer = stack(d3.transpose(d3.range(20).map(function() {return bumps(100, 20); })));
+var stack = d3.stack().keys(d3.range(10)).offset(d3.stackOffsetWiggle), layer = stack(d3.transpose(d3.range(20).map(function() {return 10; })));
 
 svg.selectAll("path").data(stack);
