80ea5431bf38faa462effea0e2046e6b95fb7e4a
pjleonard37 <pjleonard37@gmail.com>
2018-05-02 22:34:53 -0500

    Transitions and cleanup

--- js/main.js
+++ js/main.js
@@ -6,13 +6,7 @@ var div = d3.select("body").append("div")
     .style("opacity", 0);
 
 // Color selections
-var colorWheel = [
-    "#fbb735", "#e98931", "#eb403b", "#b32E37", "#6c2a6a",
-    "#5c4399", "#274389", "#1f5ea8", "#227FB0", "#2ab0c5",
-    "#fbb735", "#e98931", "#eb403b", "#b32E37", "#6c2a6a",
-    "#5c4399", "#274389", "#1f5ea8", "#227FB0", "#2ab0c5",
-    "#5c4399", "#274389", "#1f5ea8", "#39c0b3"
-];
+var colorWheel = ['#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5','#ffed6f','#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928'];
 
 // Read in data
 d3.csv(csv, function(error, d){
@@ -79,8 +73,6 @@ function buildGraph(fullData) {
     return d3.min(series, function(d) { return d[0]; });
   }
 
-  var z = d3.interpolateCool;
-
   var area = d3.area()
     .x(function(d) { return x(d.data.Year); })
     .y0(function(d) { return y(d[0]); })
@@ -97,15 +89,14 @@ function buildGraph(fullData) {
 
   // Tooltips, lines, and other chart ornaments
   var line = svg.append("line")
-    .style("stroke-width", 20)
+    .style("stroke-width", 15)
     .style("stroke", "grey")
     .style("opacity", 0);
 
   svg.selectAll(".layer")
     .attr("opacity", 1)
     .on("mousemove", function(d, i) {
-      svg.selectAll(".layer").transition()
-      .duration(250)
+      svg.selectAll(".layer")
       .attr("opacity", function(d, j) {
         return j != i ? 0.6 : 1;
       })
@@ -114,46 +105,69 @@ function buildGraph(fullData) {
       mouseYear = mouseYear[0];
       var invertMouse = x.invert(mouseYear);
 
+      line
+        .attr("x1", x(invertMouse)-10)
+        .attr("y1", 0)
+        .attr("x2", x(invertMouse)-10)
+        .attr("y2", height)
+        .style("opacity", .3);
+
       var countryDisplay = d.key;
       var yearDisplay = invertMouse.getFullYear();
       var countDisplay = data[invertMouse.getFullYear()-1955][d.key];
 
-      div	.html(countryDisplay + "<br>" + yearDisplay +  "<br>" + countDisplay)
-      .style("left", (d3.event.pageX) + "px")
-      .style("top", (d3.event.pageY - 28) + "px");
+      // Thousands
+      const addCommas = (x) => {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      }
+      countDisplay = addCommas(countDisplay);
 
-      line
-        .attr("x1", x(invertMouse))
-        .attr("y1", 0)
-        .attr("x2", x(invertMouse))
-        .attr("y2", height)
-        .style("opacity", .3);
+      div
+        .html("<div class='country'>" + countryDisplay + "</div><div class='year'>" + yearDisplay +  "</div><div class='count'>" + countDisplay + " Immigrants</div>")
+        .style("left", (d3.event.pageX + 28) + "px")
+        .style("top", (d3.event.pageY - 28) + "px");
 
       div.transition()
         .duration(250)
         .style("opacity", 1);
     })
 
+  // Revert when mouse leaves
+  d3.select("svg")
+    .on("mouseleave", function(d,i) {
+      line.transition()
+        .duration(250)
+        .style("opacity", 0);
+      div.transition()
+        .duration(250)
+        .style("opacity", 0);
+      svg.selectAll(".layer").transition()
+        .duration(250)
+        .attr("opacity", function(d, j) {
+          return j != i ? 1 : .6;
+        })
+    })
+
   // Build Axes
   var xAxis = d3.axisBottom()
-      .scale(x)
-      .ticks(10);
+    .scale(x)
+    .ticks(10);
 
   var yAxis = d3.axisLeft()
-      .scale(y)
-      .ticks(10);
+    .scale(y)
+    .ticks(10);
 
   // Add the X Axis
   svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
 
   // Add the Y Axis
   svg.append("g")
-      .attr("class", "y axis")
-      .attr("transform", "translate(" + margin.right + ",0)")
-      .call(yAxis);
+    .attr("class", "y axis")
+    .attr("transform", "translate(" + margin.right + ",0)")
+    .call(yAxis);
 
   // Add the legend
   var legend = svg.append("g")
@@ -163,7 +177,7 @@ function buildGraph(fullData) {
     .attr("heigth", 100)
     .attr("width", 100);
 
-  console.log(series);
+  // Reverse for legend
   var reverseSeries = series.reverse();
   legend.selectAll('g').data(reverseSeries)
     .enter()
